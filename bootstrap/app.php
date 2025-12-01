3c88e4fb529ad3ac2ed134a481ab7e6a131dd932
Sam <ijsglijder@gmail.com>
2025-12-01 20:37:43 +0100

    Loading env via getenv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,19 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$envPath = env('APP_ENV_PATH') ? env('APP_ENV_PATH') : dirname(__DIR__);
-
-$app = new Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-$app->useEnvironmentPath($envPath);
-
-if (env('APP_STORAGE_PATH')) {
-    $app->useStoragePath(env('APP_STORAGE_PATH'));
-}
-
-return $app->configure(base_path: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -28,3 +16,17 @@ return $app->configure(base_path: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$customEnvPath = getenv('APP_ENV_PATH');
+
+if ($customEnvPath) {
+    $app->useEnvironmentPath($customEnvPath);
+}
+
+$customStoragePath = getenv('APP_STORAGE_PATH');
+
+if ($customStoragePath) {
+    $app->useStoragePath($customStoragePath);
+}
+
+return $app;
