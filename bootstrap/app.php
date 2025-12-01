02cda86355b969fe2904dba8c75d603774953991
Sam <samvanderlugt@outlook.com>
2025-12-01 20:14:49 +0100

    Should now work with docker and git-sync

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,17 +4,27 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app->useStoragePath(env('APP_STORAGE_PATH', base_path('storage')));
+$envPath = env('APP_ENV_PATH') ? env('APP_ENV_PATH') : dirname(__DIR__);
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = new Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+$app->useEnvironmentPath($envPath);
+
+if (env('APP_STORAGE_PATH')) {
+    $app->useStoragePath(env('APP_STORAGE_PATH'));
+}
+
+return $app->configure(base_path: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
